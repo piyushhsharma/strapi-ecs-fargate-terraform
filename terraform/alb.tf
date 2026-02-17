266ef318bff5bac9ed4f0a73fca80e2079d4daa5
Piyush Sharma <piyush.jpg.32@gmail.com>
2026-02-17 18:15:55 +0530

    Fix ALB health check arguments and add proper .gitignore

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -13,12 +13,12 @@ resource "aws_lb_target_group" "this" {
   target_type = "ip"
   
   health_check {
-    path              = "/"
-    protocol          = "HTTP"
-    matcher           = "200"
-    interval_seconds  = 30
-    timeout_seconds   = 5
-    healthy_threshold = 2
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
     unhealthy_threshold = 2
   }
 }
